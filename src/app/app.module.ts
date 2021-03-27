a0f0f6b3f86218cd9ddad679a840f763469808bd
Charles Santos <charles.santos-ext@viavarejo.com.br>
2021-03-27 12:07:31 -0300

    criar modulos components, interfaces e services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components/components.module';
+import { InterfacesModule } from './interfaces/interfaces/interfaces.module';
+import { ServicesModule } from './services/services/services.module';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ComponentsModule,
+    InterfacesModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
