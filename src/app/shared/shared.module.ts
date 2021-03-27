5e4bfc7f062422ee95365143a7b525ede9a58e34
Charles Santos <charles.santos-ext@viavarejo.com.br>
2021-03-27 15:56:41 -0300

    remover models

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
-import { InterfacesModule } from 'src/app/shared/interfaces/interfaces.module';
 import { ServicesModule } from 'src/app/shared/services/services.module';
-import { EnumModule } from './enum/enum.module';
-import { ModelsModule } from './models/models.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ComponentsModule,
-    InterfacesModule,
     ServicesModule,
-    EnumModule,
-    ModelsModule
   ]
 })
 export class SharedModule { }
