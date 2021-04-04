c4e4df9bd94ea6d0f104a750274d42ca017db39a
Charles Santos <charles.santos-ext@viavarejo.com.br>
2021-04-04 16:55:26 -0300

    add cabecalho

---
+++ src/app/shared/components/cabecalho/cabecalho.component.ts
@@ -0,0 +1,20 @@
+import { Location } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.scss']
+})
+export class CabecalhoComponent {
+
+  @Input() textoCabecalho: string;
+
+  constructor(
+    private location: Location
+  ) { }
+
+  selecionarBotao(): void {
+    this.location.back();
+  }
+}
